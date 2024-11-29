1beb5de7bef76a95be9368d331927151699be638
leonardosf98 <leonardo-f98@hotmail.com>
2024-11-29 16:15:39 -0300

    feat: implements task repository

---
+++ src/main/java/io/github/leonardosf98/todolist/task/TaskRepository.java
@@ -0,0 +1,9 @@
+package io.github.leonardosf98.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
