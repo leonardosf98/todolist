a41c509afdf57199923a03fcf97569eb9a2f02a5
leonardofc <leonardo.souza@fcamara.com>
2024-11-30 20:23:15 -0300

    feat: implements get all tasks from specific user

--- src/main/java/io/github/leonardosf98/todolist/task/TaskController.java
+++ src/main/java/io/github/leonardosf98/todolist/task/TaskController.java
@@ -3,22 +3,26 @@ package io.github.leonardosf98.todolist.task;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
+    @GetMapping("/")
+    public List<TaskModel> listALl(HttpServletRequest request) {
+        var userId = request.getAttribute("idUser");
+        return this.taskRepository.findByIdUser((UUID) userId);
+    }
+
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping("/create")
+    @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         try {
             LocalDateTime currrentDate = LocalDateTime.now();
@@ -27,8 +31,8 @@ public class TaskController {
             } else if (taskModel.getEndAt().isBefore(taskModel.getStartAt())) {
                 return ResponseEntity.badRequest().body("A data de início deve ser menor do que a data de término");
             }
-            TaskModel taskToAdd = this.taskRepository.save(taskModel);
             taskModel.setIdUser((UUID) request.getAttribute("idUser"));
+            TaskModel taskToAdd = this.taskRepository.save(taskModel);
             return ResponseEntity.ok().body("Criado com sucesso\n" + taskToAdd);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
